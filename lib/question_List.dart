3d651c96aa61ef0979fedaf16cebff236c6fa7d6
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2021-12-27 16:20:14 +0100

    add some biology-question to test the app

---
+++ lib/question_List.dart
@@ -0,0 +1,139 @@
+List<Map<String, Object>> question = [
+  //1
+  {
+    "question_Text":
+        " Saurer Regen ist ein ernstzunehmendes Umweltproblem. Eine Regenwasserprobe in den Alpen hatte eine H+ Konzentration von 100 µmol/L. Der pH-Wert des Regenwasserswar daher ",
+    "answer_Text": [
+      {"Text": "0", "Score": 0},
+      {"Text": "1", "Score": 0},
+      {"Text": "3", "Score": 0},
+      {"Text": "4", "Score": 10},
+      {"Text": "100", "Score": 0}
+    ]
+  },
+  //2
+  {
+    "question_Text":
+        "Wenn das Wasser in einem See einen pH-Wert von 5 aufweist, welche HydroxidIonen Konzentration käme in dem See vor?",
+    "answer_Text": [
+      {"Text": "10^-9", "Score": 10},
+      {"Text": "10^5", "Score": 0},
+      {"Text": "10^-10", "Score": 0},
+      {"Text": "10^9", "Score": 0},
+      {"Text": "10^-5", "Score": 0}
+    ]
+  },
+  //3
+  {
+    "question_Text":
+        " Ordne die Elemente Kohlenstoff (C), Wasserstoff (H), Sauerstoff (O), Stickstoff (N) und Phosphor (P) nach Anzahl von kovalenten Bindungen, die diese Elemente eingehen können. Das erste Element sollte die höchste Anzahl von kovalenten Bindungen eingehen können.",
+    "answer_Text": [
+      {"Text": "C > P > N > O > H", "Score": 0},
+      {"Text": "P > O > C > N > H", "Score": 0},
+      {"Text": "P > C > O > N > H", "Score": 0},
+      {"Text": "P > C > N > O > H", "Score": 10},
+      {"Text": "P > C > O > H > N", "Score": 0}
+    ]
+  },
+  //4
+  {
+    "question_Text": " Welche der folgenden Aussage ist richtig?",
+    "answer_Text": [
+      {
+        "Text":
+            "A) Stickstoff bildet genauso viele kovalente Bindungen wie Sauerstoff",
+        "Score": 0
+      },
+      {
+        "Text": "B) Sauerstoff bildet mehr kovalente Bindungen als Schwefel",
+        "Score": 0
+      },
+      {
+        "Text": "C) Stickstoff bildet mehr kovalente Bindungen als Kohlenstoff",
+        "Score": 0
+      },
+      {
+        "Text":
+            "D) Sauerstoff bildet weniger kovalente Bindungen als Stickstoff",
+        "Score": 10
+      },
+      {
+        "Text": "E) Wasserstoff bildet mehr kovalente Bindungen als Sauerstoff",
+        "Score": 0
+      }
+    ]
+  },
+  //5
+  {
+    "question_Text":
+        "Welche der folgenden funktionellen Gruppen beinhaltet den am höchsten oxidierten Kohlenstoff?",
+    "answer_Text": [
+      {"Text": "A) Hydroxyl-Gruppe", "Score": 10},
+      {"Text": "B) Aldehyd-Gruppe", "Score": 0},
+      {"Text": "C) Keto-Gruppe", "Score": 0},
+      {"Text": "D) Amino-Gruppe", "Score": 0},
+      {"Text": "E) Carboxyl-Gruppe", "Score": 0}
+    ]
+  },
+  //6
+  {
+    "question_Text":
+    "Welche der folgenden funktionellen Gruppen beinhaltet den am höchsten oxidierten Kohlenstoff?",
+    "answer_Text": [
+      {"Text": "A) Hydroxyl-Gruppe", "Score": 10},
+      {"Text": "B) Aldehyd-Gruppe", "Score": 0},
+      {"Text": "C) Keto-Gruppe", "Score": 0},
+      {"Text": "D) Amino-Gruppe", "Score": 0},
+      {"Text": "E) Carboxyl-Gruppe", "Score": 0}
+    ]
+  },
+  //7
+  {
+    "question_Text":
+    "Welche der folgenden funktionellen Gruppen beinhaltet den am höchsten oxidierten Kohlenstoff?",
+    "answer_Text": [
+      {"Text": "A) Hydroxyl-Gruppe", "Score": 10},
+      {"Text": "B) Aldehyd-Gruppe", "Score": 0},
+      {"Text": "C) Keto-Gruppe", "Score": 0},
+      {"Text": "D) Amino-Gruppe", "Score": 0},
+      {"Text": "E) Carboxyl-Gruppe", "Score": 0}
+    ]
+  },
+  //8
+  {
+    "question_Text":
+    "Welche der folgenden funktionellen Gruppen beinhaltet den am höchsten oxidierten Kohlenstoff?",
+    "answer_Text": [
+      {"Text": "A) Hydroxyl-Gruppe", "Score": 10},
+      {"Text": "B) Aldehyd-Gruppe", "Score": 0},
+      {"Text": "C) Keto-Gruppe", "Score": 0},
+      {"Text": "D) Amino-Gruppe", "Score": 0},
+      {"Text": "E) Carboxyl-Gruppe", "Score": 0}
+    ]
+  },
+  //9
+  {
+    "question_Text":
+    "Welche der folgenden funktionellen Gruppen beinhaltet den am höchsten oxidierten Kohlenstoff?",
+    "answer_Text": [
+      {"Text": "A) Hydroxyl-Gruppe", "Score": 10},
+      {"Text": "B) Aldehyd-Gruppe", "Score": 0},
+      {"Text": "C) Keto-Gruppe", "Score": 0},
+      {"Text": "D) Amino-Gruppe", "Score": 0},
+      {"Text": "E) Carboxyl-Gruppe", "Score": 0}
+    ]
+  },
+  //10
+  {
+    "question_Text":
+    "Welche der folgenden funktionellen Gruppen beinhaltet den am höchsten oxidierten Kohlenstoff?",
+    "answer_Text": [
+      {"Text": "A) Hydroxyl-Gruppe", "Score": 10},
+      {"Text": "B) Aldehyd-Gruppe", "Score": 0},
+      {"Text": "C) Keto-Gruppe", "Score": 0},
+      {"Text": "D) Amino-Gruppe", "Score": 0},
+      {"Text": "E) Carboxyl-Gruppe", "Score": 0}
+    ]
+  },
+  //11
+];
